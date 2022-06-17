29c8871d46e14abf263b1711f68d2b3cc33a8785
javohir-2003 <jruzimuratov13@gmail.com>
2022-06-17 07:03:37 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// 1-masala 
+function addition(a, b) {
+  return a + b;
+  }
+// 2-masala 
+function convert(minutes) {
+  return minutes*60;
+  }
+// 3-masala 
+function addition(num) {
+  return ++num;
+  }
+// 4-masala 
+function triArea(base, height) {
+  return base*height/2
+  }
+// 5-masala 
+function calcAge(age) {
+  return age*365;
+  }
+// 6-masala 
+function remainder(x, y) {
+  return x%y;
+  }
+// 7-masala 
+function footballPoints(wins, draws, losses) {
+  return wins*3 + draws * 1;
+  }
+// 8-masala 
+function points(twoPointers, threePointers) {
+  return twoPointers*2 + threePointers*3;
+  }
+// 9-masala 
+function lessThan100(a, b) {
+  if(a + b < 100)
+  {
+       return true; 
+  }
+  else
+  {
+       return false;
+   }
+       }
+// 10-masala 
+function animals(chickens, cows, pigs)
+{
+    return chickens * 2 + (cows + pigs) * 4;
+}
